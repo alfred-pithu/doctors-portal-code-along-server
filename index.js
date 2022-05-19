df089af0299d039ddd3a45075af48a38c1a60568
unknown <alfredpithu@gmail.com>
2022-05-20 04:16:06 +0600

    module 75 completed

--- index.js
+++ index.js
@@ -2,32 +2,46 @@ const express = require('express');
 const app = express()
 const port = process.env.PORT || 5000;
 const cors = require('cors');
-require('dotenv').config;
+require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken')
 
 //middleware
 app.use(cors())
 app.use(express.json())
 
-//-------------------------------------------------------------------------
-// ${process.env.DB_USER}
-// ${process.env.DB_PASS}
+//-------------------------------------------------------------------
 
-///////// environment file er theke neoa id password process.env akare use korle server e problem hocche , kaj kortese na !!!!!!!!!!!!!
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.v7gqx.mongodb.net/?retryWrites=true&w=majority`
 
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.v7gqx.mongodb.net/?retryWrites=true&w=majority`
-//------------------------------------------------------------------------
-
-const uri = 'mongodb+srv://doctor_admin:MHgVYl5hU7aNKWbm@cluster0.v7gqx.mongodb.net/?retryWrites=true&w=majority'
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 })
+
+//verify token here
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'UnAuthorized Access' })
+    }
+
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
+        };
+
+        req.decoded = decoded;
+        next();
+    });
+}
 
 async function run() {
     try {
         await client.connect();
         console.log('database connected');
         const serviceCollection = client.db('doctors_portal').collection('services')
-        const bookingCollection = client.db('doctors_portal').collection('bookings')
+        const bookingCollection = client.db('doctors_portal').collection('bookings');
+        const userCollection = client.db('doctors_portal').collection('users');
 
 
         //getting all the services
@@ -36,15 +50,67 @@ async function run() {
             const cursor = serviceCollection.find(query)
             const services = await cursor.toArray()
             res.send(services);
+        });
+
+        app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+            const requester = req.decoded.email;
+            const requesterAccount = await userCollection.findOne({ email: requester });
+            if (requesterAccount.role === 'admin') {
+                const filter = { email: email }
+                const updateDoc = {
+                    $set: { role: 'admin' },
+                };
+                const result = await userCollection.updateOne(filter, updateDoc)
+
+                res.send(result)
+            }
+            else {
+                res.status(403).send({ message: 'Forbidden' })
+            }
+
         })
 
+        app.get('/user', verifyJWT, async (req, res) => {
+            const users = await userCollection.find().toArray();
+            res.send(users);
+        })
+
+        app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = await userCollection.findOne({ email: email })
+            const isAdmin = user.role === 'admin'
+            res.send({ admin: isAdmin })
+        })
+
+        //storing+updating users
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email }
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options)
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ result, token })
+        })
+
+        app.get('/user', verifyJWT, async (req, res) => {
+            const users = await userCollection.find().toArray();
+            res.send(users);
+        })
+
+
+        // process.env.ACCESS_TOKEN_SECRET
         // warning !!!!!!!!!!!!
         // this is not the proper way to query
         // after learning more about more about mongodb . use aggregate lookup , pipeline , match , group
 
         app.get('/available', async (req, res) => {
             const date = req.query.date
-            console.log(req.query);
+            // console.log(req.query);
 
             //step 1: get all services
             const services = await serviceCollection.find().toArray();
@@ -74,15 +140,22 @@ async function run() {
      * app.get('/booking/:id') //get a specific booking
      * app.post('/booking')  // add a new booking
      * app.patch('/booking/:id')
+     * app.put('/booking:id') //upsert ==> 
      * app.delete('/booking/:id')
      */
 
         //for getting each users bookings/appointments
-        app.get('/booking', async (req, res) => {
+        app.get('/booking', verifyJWT, async (req, res) => {
             const patient = req.query.patient;
-            const query = { patient: patient }
-            const bookings = await bookingCollection.find(query).toArray();
-            res.send(bookings)
+            const decodedEmail = req.decoded.email;
+            if (patient === decodedEmail) {
+                const query = { patient: patient };
+                const bookings = await bookingCollection.find(query).toArray();
+                res.send(bookings)
+            }
+            else {
+                res.status(403).send({ message: 'Forbidden access' })
+            }
         })
 
         //for saving user's booking info to the db
